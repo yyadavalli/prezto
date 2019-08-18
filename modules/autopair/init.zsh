d0fb60b31aa4060e44d51013abc2491bbeab474b
Yashwanth Yadavalli <yashwanth.yadavalli@protonmail.com>
2019-08-18 13:40:27 -0400

    Integrate new plugins autopair & tipz as modules

---
+++ modules/autopair/init.zsh
@@ -0,0 +1,9 @@
+#
+# Integrates zsh-autopair into Prezto.
+#
+# Authors:
+#   Yashwanth Yadavalli <yashwanth.yadavalli@protonmail.com>
+#
+
+# Source module files.
+source "${0:h}/external/autopair.zsh" || return 1
---
+++ modules/tipz/init.zsh
@@ -0,0 +1,9 @@
+#
+# Integrates Tipz into Prezto.
+#
+# Authors:
+#   Yashwanth Yadavalli <yashwanth.yadavalli@protonmail.com>
+#
+
+# Source module files.
+source "${0:h}/external/tipz.zsh" || return 1
