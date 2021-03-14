9ab4621d0e2dd83f38b5ac736ca470516bdf1a77
Yashwanth Yadavalli <yashwanth.yadavalli@protonmail.com>
2021-03-14 13:34:45 -0700

    Replace zsh-z with wdx

---
+++ modules/wdx/init.zsh
@@ -0,0 +1,9 @@
+#
+# Integrates wdx into Prezto.
+#
+# Authors:
+#   Yashwanth Yadavalli <yashwanth.yadavalli@protonmail.com>
+#
+
+# Source module files.
+source "${0:h}/external/wdx.zsh" || return 1
